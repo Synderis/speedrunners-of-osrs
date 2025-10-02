efedfc9919ff7236d943aee3d55c39972b150f64
synderis <toccidylan@gmail.com>
2025-10-02 01:03:55 -0400

    update to scaling and gear update

--- drop_all_supabase_tables.sql
+++
@@ -1,7 +0,0 @@
--- Drop all related tables in the correct order to avoid foreign key issues
--- Run this in the Supabase SQL editor
-
-drop table if exists weapon cascade;
-drop table if exists equipment cascade;
-drop table if exists items cascade;
-drop table if exists monsters cascade;
